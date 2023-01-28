f7fc8f4ed223ad4975e368ef9850812204c7b35e
Ananas <mrs.anna.noga@gmail.com>
2023-01-28 21:48:49 +0000

    city inputs are built up in the history column below search box

--- assets/script.js
+++ assets/script.js
@@ -27,8 +27,45 @@ header.css("background-image", 'url("images/clouds.jpg")');
 
 var clearBtn = $("<button>")
 clearBtn.attr("id", "clear-button");
-clearBtn.attr("class", "btn btn-primary mb-3");
+clearBtn.attr("class", "btn btn-outline-primary mb-3");
 clearBtn.text("Clear history");
 
 $("aside").append(clearBtn);
+
+//assing API key , city input and query URL variables
+var cityInput = searchInput.val();
+var apiKey = "668f0e3927f32d39fd3217f656c36bbd";
+var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + cityInput + "&appid=" + apiKey;
+
+//render history buttons
+
+var cities = [];
+
+function renderHistoryButtons(){
+
+    $("#history").empty();
+    for (var i = 0; i < cities.length; i++) {
+  
+      var newHistoryButton = $("<button>");
+      newHistoryButton.text(cities[i])
+      newHistoryButton.attr("id", cityInput)
+      newHistoryButton.addClass("btn btn-primary");
+      newHistoryButton.css("margin-bottom", "5px");
+        
+      $("#history").append(newHistoryButton);
+  
+  
+    } 
+};
+
+// search button event listener for buiding history buttons
+
+searchBtn.on("click", function (event) {
+
+    event.preventDefault();
+    var cityInput = searchInput.val();
+    cities.push(cityInput);
+  
+    renderHistoryButtons();
+  });
 
