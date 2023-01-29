76fe4766ea667dabbf75e338701bfe74a47aea36
Ananas <mrs.anna.noga@gmail.com>
2023-01-29 23:07:27 +0000

    search input srored in local storage, when refreshed, list of saved cities appear when new city input, need to change that later

--- assets/script.js
+++ assets/script.js
@@ -36,11 +36,13 @@ $("aside").append(clearBtn);
 //render history buttons
 var cityInput = searchInput.val();
 var cities = [];
+cities = JSON.parse(localStorage.getItem("City History")) || [];   //check this again, to make buttons seen after refreshing page
+var historyList = $("#history");
 
 function renderHistoryButtons() {
 
     $("#history").empty();
-
+    
 
     for (var i = 0; i < cities.length; i++) {
 
@@ -51,25 +53,36 @@ function renderHistoryButtons() {
         newHistoryButton.attr("id", cityInput)
         newHistoryButton.addClass("btn btn-primary history-button");
         newHistoryButton.css("margin-bottom", "5px");
+        
+        historyList.append(newHistoryButton);
 
         newHistoryButton.on("click", function () {
 
             displayWeather(newHistoryButton.val());
         });
-        $("#history").append(newHistoryButton);
-
 
     }
+    
 };
 
-// search button event listener for buiding history buttons
+// search button event listener for buiding history buttons, store data in local storage
+//let cities = JSON.parse(localStorage.getItem("City History")) || [];
 
 searchBtn.on("click", function (event) {
 
     event.preventDefault();
     var cityInput = searchInput.val();
     cities.push(cityInput);
+    localStorage.setItem("City History", JSON.stringify(cities));
 
+    renderHistoryButtons();
+    
+});
+
+//clear button function
+clearBtn.on("click", function () {
+    localStorage.clear();
+    cities = [];
     renderHistoryButtons();
 });
 
@@ -89,6 +102,8 @@ function displayWeather() {
     }).then(function (response) {
         console.log(response);
     })
+
+
 
 
 };
