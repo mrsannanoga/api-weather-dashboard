3e8c80ee4a288ecaf7d5d1dc60f248cb19a827b7
Ananas <mrs.anna.noga@gmail.com>
2023-01-29 00:57:40 +0000

    data request called via search button or history button

--- assets/script.js
+++ assets/script.js
@@ -4,16 +4,17 @@ searchBtn.text("");
 searchBtn.removeClass("search-button");
 searchBtn.addClass("btn-primary");
 searchBtn.css({
-    "border-top-right-radius":"0.25rem", 
-    "border-bottom-right-radius":"0.25rem"});
+    "border-top-right-radius": "0.25rem",
+    "border-bottom-right-radius": "0.25rem"
+});
 
-    //create search icon
+//create search icon
 var searchIcon = $("<i>");
 searchIcon.attr("class", "fas fa-search");
 searchIcon.css("color", "white");
 searchBtn.append(searchIcon);
-    
-    //add class to form input, change its placeholder text
+
+//add class to form input, change its placeholder text
 var searchInput = $("#search-input");
 searchInput.addClass("form-control");
 searchInput.attr("placeholder", "Choose a city");
@@ -32,30 +33,33 @@ clearBtn.text("Clear history");
 
 $("aside").append(clearBtn);
 
-//assing API key , city input and query URL variables
-var cityInput = searchInput.val();
-var apiKey = "668f0e3927f32d39fd3217f656c36bbd";
-var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + cityInput + "&appid=" + apiKey;
-
 //render history buttons
-
+var cityInput = searchInput.val();
 var cities = [];
 
-function renderHistoryButtons(){
+function renderHistoryButtons() {
 
     $("#history").empty();
+
+
     for (var i = 0; i < cities.length; i++) {
-  
-      var newHistoryButton = $("<button>");
-      newHistoryButton.text(cities[i])
-      newHistoryButton.attr("id", cityInput)
-      newHistoryButton.addClass("btn btn-primary");
-      newHistoryButton.css("margin-bottom", "5px");
-        
-      $("#history").append(newHistoryButton);
-  
-  
-    } 
+
+
+        var cityInput = searchInput.val();
+        var newHistoryButton = $("<button>");
+        newHistoryButton.text(cities[i])
+        newHistoryButton.attr("id", cityInput)
+        newHistoryButton.addClass("btn btn-primary history-button");
+        newHistoryButton.css("margin-bottom", "5px");
+
+        newHistoryButton.on("click", function () {
+
+            displayWeather(newHistoryButton.val());
+        });
+        $("#history").append(newHistoryButton);
+
+
+    }
 };
 
 // search button event listener for buiding history buttons
@@ -65,7 +69,30 @@ searchBtn.on("click", function (event) {
     event.preventDefault();
     var cityInput = searchInput.val();
     cities.push(cityInput);
-  
+
     renderHistoryButtons();
-  });
+});
+
+//weather data request
+function displayWeather() {
+
+    // var myClickBtn = $(event.target);
+    // var cityName = myClickBtn.cityInput;
+
+    var cityInput = searchInput.val();
+    var apiKey = "668f0e3927f32d39fd3217f656c36bbd";
+    var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + cityInput + "&appid=" + apiKey;
+
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    }).then(function (response) {
+        console.log(response);
+    })
+
+
+};
+
+searchBtn.on("click", displayWeather);
+
 
